16fb650c407f10e791c12dfa1890bb3781e00139
andy5995 <arch_stanton5995@proton.me>
2025-02-19 00:18:08 -0600

    Add arithmetic md5sum checks and enable test, build static lib

---
+++ tests/md5sum-wrapper.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$MESON_CURRENT_BUILD_DIR" ]; then
+  echo "This script used by Meson for testing."
+fi
+
+cd "$MESON_CURRENT_BUILD_DIR"
+$1 --warn --check "$MESON_CURRENT_SOURCE_DIR/$MESON_TEST.md5"
